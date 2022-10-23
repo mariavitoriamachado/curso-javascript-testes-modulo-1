1bc5dd3bb1c0123e7073f8ea83ae870d3f054d33
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2022-10-22 21:44:46 -0300

    chore: More refactoring with test support

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,10 +1,10 @@
-module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => {
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        throw new Error('Please, check yout params');
-      }
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please, check yout params');
+  }
 
-      return `${key}=${value}`;
-    })
-    .join('&');
+  return `${key}=${value}`;
+};
+
+module.exports.queryString = obj =>
+  Object.entries(obj).map(keyValueToString).join('&');
