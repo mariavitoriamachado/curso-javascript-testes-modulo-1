65c0b6a493e05d9856b849305e21de43e80ec927
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2022-10-22 21:34:30 -0300

    feature: More test cases for query string

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,4 +9,28 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Maria&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Maria',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Maria&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object passed as value', () => {
+    const obj = {
+      name: 'Maria',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    console.log(queryString(obj));
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
