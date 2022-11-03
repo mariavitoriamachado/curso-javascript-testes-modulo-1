f2704ad1bc4f1b583c5c251ba9fe8e5a69ee610b
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2022-11-02 22:40:20 -0300

    feature:  Query string to object  2/2

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -36,11 +36,29 @@ describe('Object to query string', () => {
 
 describe('Query string to object', () => {
   it('should convert a query string to object', () => {
+    const qs = 'name=Maria&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Maria',
+      profession: 'developer'
+    });
+  });
+
+  it('should convert a query string of a single key-value pair to object', () => {
     const qs = 'name=Maria';
 
     expect(parse(qs)).toEqual({
       name: 'Maria',
+    });
+  });
 
+
+  it('should convert a query string to an object taking care of como separated values', () => {
+    const qs = "name=Maria&abilities=JS,TDD"
+
+    expect(parse(qs)).toEqual({
+      name: 'Maria',
+      abilities: ['JS','TDD']
     });
   });
 });
